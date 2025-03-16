332eccf47a1161e0ef4e7437140f0cfbb8325cc5
vanbasx <ivan.bas.vb@gmail.com>
2025-03-16 16:13:34 +0000

    up

--- waybar/scripts/disk.sh
+++ waybar/scripts/disk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Информация о дисках
 root_usage=$(df -h / | awk 'NR==2 {print $3 " / " $2 " (" $5 ")"}')
-# storage_usage=$(df -h /storage | awk 'NR==2 {print $3 " / " $2 " (" $5 ")"}')
+storage_usage=$(df -h /storage | awk 'NR==2 {print $3 " / " $2 " (" $5 ")"}')
 
 # Вывод для Waybar
-echo -e "󰆼 Root: $root_usage"
+echo -e "󰆼 Root: $root_usage Storage: $storage_usage"
